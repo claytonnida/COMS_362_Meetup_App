dde1a6f2ef74d69085fcc025734ac4845e6c0480
Chan Davis <chandlerfdavis@gmail.com>
2019-03-31 17:50:49 -0500

    Implement leaveGroup and filterOnlineConnections use cases + documenting
    - Cleaned up some stuff, as well
    - Added more comments to the Profile class and upgraded existing ones with fancy annotations

---
+++ src/app/Controllers/GroupAssociationController.java
@@ -0,0 +1,12 @@
+package app.Controllers;
+
+import app.MySQL.MySQLHelper;
+import app.interfaces.GroupAssociationControllerInterface;
+
+public class GroupAssociationController implements GroupAssociationControllerInterface {
+
+    @Override
+    public void leaveGroup(int accountId, int groupId) {
+        // TODO: Remove the group association with the given account ID and group ID
+    }
+}
